4800940728996835566ebadf9ed4c4a19c7d0ae8
Thurein Thant <thureinthant345@gmail.com>
2026-03-26 21:04:09 +0000

    hero sectin

---
+++ components/hero-section.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
+export default function HeroSection() {
+    return (
+        <section className="w-full py-15">
+            <div className="container mx-auto text-center">
+                {/* Title */}
+                <h1 className="text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
+                    Discover the World of Anime
+                </h1>
+
+                {/* Subtitle */}
+                <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 max-w-2xl mx-auto">
+                    Explore popular anime series, read detailed articles and
+                    dive into the stories behind your favorite characters.
+                </p>
+
+                {/* Action Buttons */}
+                <div className="flex flex-col sm:flex-row gap-6 justify-center mt-10">
+                    <Button className="text-lg py-6 px-4" asChild>
+                        <Link href="/anime">Browse Anime</Link>
+                    </Button>
+
+                    <Button className="text-lg py-6 px-4" asChild>
+                        <Link href="/articles">Latest Articles</Link>
+                    </Button>
+                </div>
+            </div>
+        </section>
+    )
+}
